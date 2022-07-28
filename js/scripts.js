7d77afc9c746ff8db48028e04b8b6cf2dcb59f26
biotoscano <biotoscano@gmail.com>
2022-07-28 08:55:09 +0200

    jquery expand/collapse avoids two sections opened simultaneuosly

--- js/scripts.js
+++ js/scripts.js
@@ -110,20 +110,27 @@ function setSelect() {
 }
 
 $(".subsubsectionHeader").click(function () {
-    
+
     $header = $(this);
+    $header[0].dataset.open = $header[0].dataset.open === "0" ? "1" : "0";
     $icon = $header.children(".closeIcon")[0];
     $content = $header.next();
     $icon.style.transform += 'rotate(180deg)';
-    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-    $content.slideToggle(500, function () {
-        //execute this after slideToggle is done
-        //change text of header based on visibility of content div
-        // $header.text(function () {
-        //     //change text based on condition
-        //     return $content.is(":visible") ? "Collapse" : "Expand";
-        // });
-    });
+
+    $otherSection = $header.parent().siblings().children();
+    $headerS = $header.parent().siblings().children(".subsubsectionHeader");
+    
+
+    if($header[0].dataset.open === "1" && $headerS[0].dataset.open === "1") {
+        $iconS = $headerS.children(".closeIcon")[0];
+        $contentS = $header.parent().siblings().children(".subsubsectionContent");
+        
+        $headerS[0].dataset.open = $headerS[0].dataset.open === "0" ? "1" : "0";
+        $contentS.slideToggle(400);
+        $iconS.style.transform += 'rotate(180deg)';
+    }
+
+    $content.slideToggle(400);
 });
 
 function setModeloInversor(e) {
